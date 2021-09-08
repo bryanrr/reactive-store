1ac281eddc384517356795ac7213def23a25462d
Bryan <bryanrr_julio_@hotmail.com>
2021-09-08 13:12:26 -0500

    Add initial config

---
+++ src/main/java/com/autoservicio/reactivestore/ReactiveStoreApplication.java
@@ -0,0 +1,13 @@
+package com.autoservicio.reactivestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/autoservicio/reactivestore/ReactiveStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autoservicio.reactivestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
